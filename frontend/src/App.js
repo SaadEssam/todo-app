b76981cc24abdef516c4e46fd342eb26f8a072f3
SaadEssam <SaadEssam059@gmail.com>
2023-01-22 16:51:08 +0200

    Enhance Code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -110,7 +110,7 @@ class App extends Component {
   render() {
     return (
       <main className="container">
-        <h1 className="text-white text-uppercase text-center my-4">Todo App</h1>
+        <h1 className="text-white text-center my-4">To-Do-List</h1>
         <div className="row">
           <div className="col-md-6 col-sm-10 mx-auto p-0">
             <div className="card p-3">
